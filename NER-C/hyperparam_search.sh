38328adcc81c75f560d0ea1cd94a65f0b43ed393
sebastian donoso <sdonoso@solotodo.com>
2021-12-06 18:26:54 -0300

    albert-large

--- NER-C/hyperparam_search.sh
+++ NER-C/hyperparam_search.sh
@@ -1,4 +1,4 @@
-batch_sizes=(32 16)
+batch_sizes=(16)
 learning_rates=(1e-5 2e-5 3e-5 5e-5)
 epochs=(2 3 4)
 
@@ -6,11 +6,11 @@ for bs in "${batch_sizes[@]}"; do
     for lr in "${learning_rates[@]}"; do
         for n_epoch in "${epochs[@]}"; do
             python run_ner.py \
-            --model_name_or_path CenIA/albert_base_spanish \
+            --model_name_or_path CenIA/albert_large_spanish \
             --max_seq_length 512 \
             --pad_to_max_length False \
             --do_lower_case True \
-            --output_dir /data/sdonoso/all_results/ner-c/albert-base/epochs_"$n_epoch"_bs_"$bs"_lr_"$lr" \
+            --output_dir /data/sdonoso/all_results/ner-c/albert-large/epochs_"$n_epoch"_bs_"$bs"_lr_"$lr" \
             --use_fast_tokenizer True \
             --language es \
             --train_language es \
@@ -20,7 +20,7 @@ for bs in "${batch_sizes[@]}"; do
             --per_device_train_batch_size "$bs" \
             --learning_rate "$lr" \
             --num_train_epochs "$n_epoch" \
-            --logging_dir /data/sdonoso/all_results/ner-c/albert-base/epochs_"$n_epoch"_bs_"$bs"_lr_"$lr" \
+            --logging_dir /data/sdonoso/all_results/ner-c/albert-large/epochs_"$n_epoch"_bs_"$bs"_lr_"$lr" \
             --seed 56 \
             --cache_dir /data/sdonoso/cache \
             --use_auth_token True \
