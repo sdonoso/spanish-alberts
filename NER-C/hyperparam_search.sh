baa72a57edaa969ef76ea018db1233784e97f644
sebastian donoso <sdonoso@solotodo.com>
2021-12-21 21:12:52 -0300

    set params beto uncased

--- NER-C/hyperparam_search.sh
+++ NER-C/hyperparam_search.sh
@@ -5,11 +5,11 @@ for bs in "${batch_sizes[@]}"; do
     for lr in "${learning_rates[@]}"; do
         for n_epoch in "${epochs[@]}"; do
             python run_ner.py \
-            --model_name_or_path CenIA/albert_tiny_spanish \
+            --model_name_or_path dccuchile/bert-base-spanish-wwm-uncased \
             --max_seq_length 512 \
             --pad_to_max_length False \
             --do_lower_case True \
-            --output_dir /data/sdonoso/all_results/ner-c/albert-tiny/epochs_"$n_epoch"_bs_"$bs"_lr_"$lr" \
+            --output_dir /data/sdonoso/all_results/ner-c/beto-uncased/epochs_"$n_epoch"_bs_"$bs"_lr_"$lr" \
             --use_fast_tokenizer True \
             --language es \
             --train_language es \
@@ -19,7 +19,7 @@ for bs in "${batch_sizes[@]}"; do
             --per_device_train_batch_size "$bs" \
             --learning_rate "$lr" \
             --num_train_epochs "$n_epoch" \
-            --logging_dir /data/sdonoso/all_results/ner-c/albert-tiny/epochs_"$n_epoch"_bs_"$bs"_lr_"$lr" \
+            --logging_dir /data/sdonoso/all_results/ner-c/beto-uncased/epochs_"$n_epoch"_bs_"$bs"_lr_"$lr" \
             --seed 56 \
             --cache_dir /data/sdonoso/cache \
             --use_auth_token True \
