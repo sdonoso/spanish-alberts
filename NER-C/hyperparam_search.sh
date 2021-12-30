668e04f2865c67187d53f14208af103d3ee18e52
sebastian donoso <sdonoso@solotodo.com>
2021-12-30 13:07:47 -0300

    set parameters distillbetyo

--- NER-C/hyperparam_search.sh
+++ NER-C/hyperparam_search.sh
@@ -1,5 +1,4 @@
-gradient_accumulation=(4)
-batch_sizes=(4)
+batch_sizes=(64 32 16)
 learning_rates=(1e-5 2e-5 3e-5 5e-5)
 epochs=(2 3 4)
 for ga in "${gradient_accumulation[@]}"; do
@@ -7,30 +6,30 @@ for ga in "${gradient_accumulation[@]}"; do
         for lr in "${learning_rates[@]}"; do
             for n_epoch in "${epochs[@]}"; do
                 python run_ner.py \
-                --model_name_or_path CenIA/albert_xxlarge_spanish \
+                --model_name_or_path CenIA/distillbert-base-spanish-uncased \
                 --max_seq_length 512 \
                 --pad_to_max_length False \
                 --do_lower_case True \
-                --output_dir /home/sdonoso/data/all_results/ner-c/albert-xxlarge/epochs_"$n_epoch"_bs_"$bs"_lr_"$lr" \
+                --output_dir /home/sdonoso/data/all_results/ner-c/distillbeto/epochs_"$n_epoch"_bs_"$bs"_lr_"$lr" \
                 --use_fast_tokenizer True \
                 --language es \
                 --train_language es \
                 --do_train \
                 --do_eval \
-                --gradient_accumulation_steps "$ga" \
                 --per_device_eval_batch_size "$bs" \
                 --per_device_train_batch_size "$bs" \
                 --learning_rate "$lr" \
                 --num_train_epochs "$n_epoch" \
-                --logging_dir /home/sdonoso/data/all_results/ner-c/albert-xxlarge/epochs_"$n_epoch"_bs_"$bs"_lr_"$lr" \
+                --logging_dir /home/sdonoso/data/all_results/ner-c/distillbeto/epochs_"$n_epoch"_bs_"$bs"_lr_"$lr" \
                 --seed 56 \
                 --cache_dir /data/sdonoso/cache \
                 --use_auth_token True \
                 --evaluation_strategy steps \
-                --save_steps 2000 \
-                --eval_steps 2000 \
+                --save_steps 50 \
+                --eval_steps 50 \
                 --load_best_model_at_end True \
                 --fp16 \
+                --overwrite_output_dir \
                 ;
             done
         done
